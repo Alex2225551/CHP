800288d9dc94c0f6dc51cd13edea01acf2ec3541
Alex Gharibyan <143819372+Alex2225551@users.noreply.github.com>
2024-10-16 13:46:32 +0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int dev(int a, int b) {
+    a = (a < 0) ? -a : a;
+    b = (b < 0) ? -b : b;
+
     while (b != 0) {
         int temp = b;
         b = a % b;
@@ -12,12 +15,12 @@ int dev(int a, int b) {
 int main() {
     int num1, num2;
 
-    printf("mutqagreq tvery ");
+    printf("Mutqagreq tvery: ");
     scanf("%d %d", &num1, &num2);
 
     int result = dev(num1, num2);
 
-    printf(" %d-i ev %d-i amenamec yndhanur bajanarary %d-n e \n", num1, num2, result);
+    printf("%d-i ev %d-i amenamec yndhanur bajanarary %d-n e\n", num1, num2, result);
 
     return 0;
 }
